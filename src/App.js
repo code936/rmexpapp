69a85707e624dedfa02a06ff76910aea12188283
Kunal Gaurav <ikunalgaurav@gmail.com>
2018-12-30 20:33:58 +0530

    Added fetch flob

--- src/App.js
+++ src/App.js
@@ -1,18 +1,37 @@
 
 
 import React, {Component} from 'react';
-import {Text, View} from 'react-native';
+import {Text, View, Button} from 'react-native';
+import RNFetchblob from "rn-fetch-blob"
 
 
 
 
 export default class App extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      dataRecieved: "Default data"
+    }
+  }
+
+  fetchData = () => {
+    RNFetchblob.config({
+      trusty: true
+    })
+      .fetch('GET', "https://localhost:8000/about")
+      .then(res => res.json())
+      .then(data => this.setState({dataRecieved: JSON.stringify(data)}))
+      .catch(error => this.setState({dataRecieved: error.message}))
+  }
+
   render () {
     return (
       <View style={{flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: '#F5FCFF'}}>
         <Text >Welcome to React Native!</Text>
-
-      </View>
+        <Button title="Fetch Data" onPress={() => this.fetchData()} />
+        <Text >{this.state.dataRecieved}</Text>
+      </View >
     );
   }
 }
